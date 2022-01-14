afec99a9db7cb64b7a8953abe3a8cd172892cbfd
italo <italo@pop-os.localdomain>
2022-01-14 14:51:19 -0300

    3.0

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
@@ -5,6 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('imovel/<str:id>', views.imovel, name='imovel')
+    path('imovel/<str:id>', views.imovel, name='imovel'),
+    path('agendar_visitas', views.agendar_visitas, name="agendar_visitas"),
+    path('agendamentos', views.agendamentos, name='agendamentos'),
+    path('cancelar_agendamento/<str:id>', views.cancelar_agendamento, name="cancelar_agendamento"),
     
 ]
--- plataforma/views.py
+++ plataforma/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Imovei, Cidade
+from .models import Imovei, Cidade, Visitas
 from django.shortcuts import get_object_or_404
+
 
 @login_required(login_url='/auth/logar')
 def home(request):
@@ -31,3 +32,29 @@ def imovel(request, id):
     sugestoes = Imovei.objects.filter(cidade=imovel.cidade).exclude(id=id)[:2]
     return render(request, 'imovel.html', {'imovel':imovel, 'sugestoes':sugestoes})
 
+def agendar_visitas(request):
+    usuario = request.user
+    dia = request.POST.get('dia')
+    horario = request.POST.get('horario')
+    id_imovel = request.POST.get('id_imovel')
+    visita = Visitas(
+        imovel_id=id_imovel,
+        usuario=usuario,
+        dia=dia,
+        horario=horario
+    )
+    visita.save()
+
+    return redirect('/agendamentos')
+
+def agendamentos(request):
+    visitas = Visitas.objects.filter(usuario=request.user)
+    return render(request, 'agendamentos.html', {'visitas': visitas})
+
+
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = "C"
+    visitas.save()
+    return redirect('/agendamentos')
